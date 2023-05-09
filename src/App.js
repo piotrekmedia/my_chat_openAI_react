0c2b89e1240cedec78ac669482fff9c078ae194b
Piotr Majewski <piotr.majewski@exorigo-upos.pl>
2023-05-09 15:50:42 +0200

    Frontend & backend package.json config + input styled added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,21 @@ const App = () => {
               <p>Made by Pioter</p>
           </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+          <h1>Pioter tell you proof</h1>
+          <ul className="feed">
+
+          </ul>
+          <div className="bottom-section">
+            <div className="input-container">
+                <input/>
+                <div id="submit">
+                    ➢
+                </div>
+                <p className="info">Because I'm creating my own project, I wanted it to look and work similar to chatGPT. Hope you like it and have fun!</p>
+            </div>
+          </div>
+      </section>
     </div>
   );
 }
